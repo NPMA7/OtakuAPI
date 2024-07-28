6c56d18612aae7773153629d459384a099dc49c2
Pasha Malik <pashamalik9371@gmail.com>
2024-07-28 14:19:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4444;
 
-// Configure CORS to allow all origins
-const corsOptions = {
-  origin: '*', // Ganti dengan domain aplikasi Anda
-  optionsSuccessStatus: 200
-};
 
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(validateApiKey);
